7e1c2fa0b36aa1e0add52189a2fc9653acb185eb
Justin Plaschka <jtplasch@gmail.com>
2021-02-11 10:54:14 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SD76_ConsoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProgramUI programUI = new ProgramUI();
+            programUI.Run();
+        }
+       
+    }
+}
